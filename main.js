8127fd67487a2f5d168030d3ddfcbfae7204c716
unknown <kunusuperit@gmail.com>
2023-02-28 15:56:56 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let name1 = document.querySelector(".name");
+let surname = document.querySelector(".surname");
+let phone = document.querySelector(".phone");
+let weekly = document.querySelector(".weekly");
+let monthly = document.querySelector(".monthly");
+let btnCreate = document.querySelector(".btn-create");
